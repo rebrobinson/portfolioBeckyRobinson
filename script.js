10896e0c0ed969b0647ce9a27a9a8f3a753b7f4c
rebrobinson <rebrobinson>
2019-06-16 17:50:51 -0400

    animations

---
+++ script.js
@@ -0,0 +1,11 @@
+$(function () {
+
+    $("h1").hover(function () {
+        $(this).toggleClass("heartbeat");
+    });
+
+    $(".skillContainer").hover(function () {
+        $(this).toggleClass("vibrate-1");
+    })
+
+})
