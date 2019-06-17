d9fc646d4228a22d0b2f8f6584eb2c2f944f405b
rebrobinson <rebrobinson>
2019-06-16 21:35:12 -0400

    hamburger menu

--- script.js
+++ script.js
@@ -1,11 +1,17 @@
 $(function () {
 
-    $("h1").hover(function () {
-        $(this).toggleClass("heartbeat");
+    $('h1').hover(function () {
+        $(this).toggleClass('heartbeat');
     });
 
-    $(".skillContainer").hover(function () {
-        $(this).toggleClass("vibrate-1");
+    $('.hiddenMenuSelect').on('click', function (event) {
+        event.preventDefault();
+        $('.mobileMenuDropdown').toggleClass('mobileMenuClicked').toggleClass('slide-in-top');
+        $(this).toggleClass('fa-rotate-90')
+    })
+
+    $('.skillContainer').hover(function () {
+        $(this).toggleClass('vibrate-1');
     })
 
 })
