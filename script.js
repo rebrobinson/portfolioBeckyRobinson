dc064991ebb189f46c081af40a9159162bfc5115
rebrobinson <rebrobinson>
2019-06-17 23:31:04 -0400

    left side mobile nav

--- script.js
+++ script.js
@@ -10,6 +10,11 @@ $(function () {
         $(this).toggleClass('fa-rotate-90')
     })
 
+    $('.leftMenuButton').on('click', function (event) {
+        event.preventDefault();
+        $('.leftMenuOpen').toggleClass('leftMenuClicked');
+    })
+
     $('.skillContainer').hover(function () {
         $(this).toggleClass('vibrate-1');
     })
